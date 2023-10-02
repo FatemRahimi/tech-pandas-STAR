9baf9db67300e6113b07e4d01ee172eec99448a5
Fatemeh Rahimi <105087850+FatemRahimi@users.noreply.github.com>
2023-10-02 16:05:20 +0100

    fixed

--- server/utils/config.js
+++ server/utils/config.js
@@ -12,16 +12,19 @@ export default {
 };
 
 function createDatabaseUrl() {
-	if (process.env.DATABASE_URL) {
-		return process.env.DATABASE_URL;
-	}
-	const host = process.env.DB_HOST ?? "localhost";
-	const name = process.env.DB_NAME ?? "cyf";
-	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "";
-	const port = process.env.DB_PORT ?? "5432";
-	const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "";
-	const userinfo = `${username}:${password}`;
-	return `postgres://${
-		userinfo !== ":" ? `${userinfo}@` : ""
-	}${host}:${port}/${name}`;
+    if (process.env.DATABASE_URL) {
+        return process.env.DATABASE_URL;
+    }
+    const host = process.env.DB_HOST ?? "localhost";
+    const name = process.env.DB_NAME ?? "cyf";
+    const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "";
+    const port = process.env.DB_PORT ?? "5432";
+    const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "";
+    const userinfo = `${username}:${password}`;
+    
+    // Check if REQUIRE_SSL is set to 'true' (string), otherwise do not include the sslmode parameter
+    const sslmode = process.env.REQUIRE_SSL === 'true' ? '?sslmode=require' : '';
+    return `postgres://${userinfo !== ":" ? `${userinfo}@` : ""}${host}:${port}/${name}${sslmode}`;
 }
+
+  
